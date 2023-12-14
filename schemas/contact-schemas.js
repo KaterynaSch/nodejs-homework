5ed15bd0c252273e7598f51bf7a7047e8ac36df4
KaterynaSch <128621831+KaterynaSch@users.noreply.github.com>
2023-12-14 20:46:29 +0200

    corrected-hw-02

--- schemas/contact-schemas.js
+++ schemas/contact-schemas.js
@@ -3,7 +3,9 @@ import Joi from "joi";
 export const contactAddSchema = Joi.object({
     name: Joi.string().required(),
     email: Joi.string().required(),
-    phone: Joi.string().required()
+    phone: Joi.string().required(),
+}).messages({
+    "any.required": 'missing required "{#label}" field',
 });
 
 //схема для оновлення, де поля не є обов'язковими
