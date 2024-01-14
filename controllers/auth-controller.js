521a6ca1853089719f30dc69b43bbf7fee861319
KaterynaSch <128621831+KaterynaSch@users.noreply.github.com>
2024-01-14 12:15:49 +0200

    corrected hw-06

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -48,13 +48,14 @@ const signup = async(req, res) => {
 const verify = async(req, res) => {
 
     const {verificationToken} = req.params; 
-    const user = await User.findOne({verificationToken});
-    const {_id} = user._id;
+    const user = await User.findOne({verificationToken});    
 
     if(!user){//перевірка чи є користувач з таким verificationToken
         throw HttpError(404, 'User not found');
     }
 
+    const {_id} = user._id;
+    
     await User.updateOne({_id}, {verify: true, verificationToken: null});
     res.status(200).json({
         message: 'Verification successful'
